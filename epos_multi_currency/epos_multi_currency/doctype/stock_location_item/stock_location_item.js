0e4d2c69b6b8bc0803afda7bcff2c714ef196f81
Pheakdey <frapperpnext@gmail.com>
2023-09-21 18:17:37 +0700

    update 2023-09-21 6:17 PM

--- epos_multi_currency/epos_multi_currency/doctype/stock_location_item/stock_location_item.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, ESTC and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Stock Location Item", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- epos_multi_currency/epos_multi_currency/doctype/stock_location_item/test_stock_location_item.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, ESTC and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestStockLocationItem(FrappeTestCase):
-	pass
