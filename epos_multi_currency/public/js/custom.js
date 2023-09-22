d830d41d1a91d402281d798d8c8c5afeec3122d5
Pheakdey <frapperpnext@gmail.com>
2023-09-22 14:09:30 +0700

    update

--- epos_multi_currency/public/js/custom.js
+++
@@ -1,7 +0,0 @@
-frappe.ui.form.on('Login', {
-    onload: function(frm) {
-        console.log(222)
-        // Hide the "Forgot Password?" link
-        frm.fields_dict['forgot_password'].wrapper.style.display = 'none';
-    }
-});
